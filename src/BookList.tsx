408b0e83594ebb5e663e45a24362c979ff0e2a82
Lawrence <lawrenceeks@yahoo.com>
2020-07-08 13:43:32 +0100

    see no books message when there is no book data

--- src/BookList.tsx
+++ src/BookList.tsx
@@ -1,9 +1,10 @@
-import React from "react"
+import React, { useState } from "react"
 import Container from "react-bootstrap/Container"
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
 import ButtonGroup from "react-bootstrap/ButtonGroup"
 import Button from "react-bootstrap/Button"
+import { useLocation } from "react-router-dom"
 
 function Header() {
   return (
@@ -14,7 +15,21 @@ function Header() {
     </Row>
   )
 }
-function Books() {
+function BooksRow({ children }: { children: React.ReactNode }) {
+  return (
+    <Row as="main" className="flex-grow-1">
+      <Col>{children}</Col>
+    </Row>
+  )
+}
+function Books({ bookData }: { bookData: any[] }) {
+  if (!bookData.length) {
+    return (
+      <BooksRow>
+        <h2>No books to show</h2>
+      </BooksRow>
+    )
+  }
   return (
     <Row as="main" className="flex-grow-1">
       <Col>
@@ -23,25 +38,53 @@ function Books() {
     </Row>
   )
 }
-function PageButtons() {
+function PageButtons({
+  prevEnabled = false,
+  nextEnabled = false,
+}: {
+  prevEnabled: boolean
+  nextEnabled: boolean
+}) {
   return (
     <Row>
       <Col>
         <ButtonGroup>
-          <Button>Prev</Button>
-          <Button>Next</Button>
+          <Button disabled={!prevEnabled}>Prev</Button>
+          <Button disabled={!nextEnabled}>Next</Button>
         </ButtonGroup>
       </Col>
     </Row>
   )
 }
 
+// hook to parse query string
+function useQuery() {
+  return new URLSearchParams(useLocation().search)
+}
+
+function parsePageNumber(queryValue: string | null) {
+  const result = Number.parseInt(queryValue || "", 10)
+  if (isNaN(result)) {
+    return 1
+  }
+  return result
+}
+
 function BookList() {
+  const [bookData, setBookData] = useState([])
+  const query = useQuery()
+
+  const pageNumber = parsePageNumber(query.get("page"))
+  const searchTerm = query.get("search")
+
   return (
     <Container className="d-flex flex-column min-vh-100">
       <Header />
-      <Books />
-      <PageButtons />
+      <Books bookData={bookData} />
+      <PageButtons
+        nextEnabled={!!bookData.length}
+        prevEnabled={!!bookData.length && pageNumber > 1}
+      />
     </Container>
   )
 }
