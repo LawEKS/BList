6120fa67b3ac624ee231aa7b1f25f224f0985d5e
Lawrence <lawrenceeks@yahoo.com>
2020-07-08 17:02:14 +0100

    request books when BookList mounts

--- cypress/integration/ui.spec.ts
+++ cypress/integration/ui.spec.ts
@@ -1,14 +1,28 @@
 describe("visible content", () => {
-  before(() => cy.visit("/"))
-  it("loads the app", () => {
-    cy.get("#root").should("be.visible")
+  it("shows welcome screen", () => {
+    cy.visit("/")
+    cy.findByText("A destination for book discovery").should("be.visible")
+    cy.findByText(/explore/i).should("be.visible")
   })
 
-  it("begin exploring books from welcome screen", () => {
-    cy.get("#root")
-      .within(() => cy.findByText(/explore/i))
-      .click()
+  it("navigates to explore route and requests for page 1 of books", () => {
+    // set up AJAX call interception
+    cy.server()
+    cy.route("POST", "**/api/books", "fixture:books_page_1").as("books-request")
+
+    cy.visit("/")
+    cy.findByText(/explore/i).click()
 
     cy.url().should("include", "explore")
+    cy.url().should("include", "page=1")
+
+    // ... and AJAX call waiting
+    cy.wait("@books-request").should((xhr) => {
+      expect(xhr.status).to.equal(200)
+      expect(xhr.response.body).to.contain.keys(["books", "count"])
+    })
+
+    cy.findByText(/prev/i).should("be.disabled")
+    cy.findByText(/next/i).should("not.be.disabled")
   })
 })
--- src/BookList.tsx
+++ src/BookList.tsx
@@ -1,10 +1,12 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import Container from "react-bootstrap/Container"
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
 import ButtonGroup from "react-bootstrap/ButtonGroup"
 import Button from "react-bootstrap/Button"
+import Spinner from "react-bootstrap/Spinner"
 import { useLocation } from "react-router-dom"
+import { postBooks, Book } from "./api"
 
 function Header() {
   return (
@@ -22,7 +24,17 @@ function BooksRow({ children }: { children: React.ReactNode }) {
     </Row>
   )
 }
-function Books({ bookData }: { bookData: any[] }) {
+function Books({ bookData, loading }: { bookData: Book[]; loading: boolean }) {
+  if (loading) {
+    return (
+      <BooksRow>
+        <Spinner animation="border" role="status">
+          <span className="sr-only">Loading...</span>
+        </Spinner>
+      </BooksRow>
+    )
+  }
+
   if (!bookData.length) {
     return (
       <BooksRow>
@@ -33,7 +45,11 @@ function Books({ bookData }: { bookData: any[] }) {
   return (
     <Row as="main" className="flex-grow-1">
       <Col>
-        <h2>Books</h2>
+        {JSON.stringify(
+          bookData.map(({ book_title }) => book_title),
+          null,
+          2,
+        )}
       </Col>
     </Row>
   )
@@ -70,19 +86,37 @@ function parsePageNumber(queryValue: string | null) {
   return result
 }
 
+function calculateLastPage(count: number, itemsPerPage: number) {
+  return Math.ceil(count / itemsPerPage)
+}
+
 function BookList() {
-  const [bookData, setBookData] = useState([])
+  const [bookData, setBookData] = useState<Book[]>([])
+  const [itemsPerPage] = useState(20)
+  const [loading, setLoading] = useState(true)
   const query = useQuery()
 
   const pageNumber = parsePageNumber(query.get("page"))
   const searchTerm = query.get("search")
 
+  const [lastPage, setLastPage] = useState(pageNumber)
+
+  useEffect(() => {
+    setLoading(true)
+    ;(async function requestBooksToRender() {
+      const data = await postBooks({ pageNumber, itemsPerPage, searchTerm })
+      setBookData(data.books)
+      setLastPage(calculateLastPage(data.count, itemsPerPage))
+      setLoading(false)
+    })()
+  }, [pageNumber, searchTerm, itemsPerPage])
+
   return (
     <Container className="d-flex flex-column min-vh-100">
       <Header />
-      <Books bookData={bookData} />
+      <Books loading={loading} bookData={bookData} />
       <PageButtons
-        nextEnabled={!!bookData.length}
+        nextEnabled={!!bookData.length && pageNumber < lastPage}
         prevEnabled={!!bookData.length && pageNumber > 1}
       />
     </Container>
