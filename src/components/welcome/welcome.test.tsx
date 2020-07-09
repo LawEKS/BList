cf50ed0cacaed4f9e250117abf6ba25a52a50598
Lawrence <lawrenceeks@yahoo.com>
2020-07-09 11:50:48 +0100

    refactor components from files into directories

--- src/components/welcome/welcome.test.tsx
+++ src/components/welcome/welcome.test.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { render } from "@testing-library/react"
 import { MemoryRouter } from "react-router-dom"
-import Welcome from "./Welcome"
+import Welcome from "."
 
 describe("Welcome content", () => {
   test("has project title", () => {
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import App from "./App"
+import App from "./components/app"
 import "./custom.scss"
 
 ReactDOM.render(
